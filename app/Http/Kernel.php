45a7e534e395ee1fa08d82304c53ffbcad476f94
rameshrajan <rajanramesh431@gmail.com>
2023-10-05 10:00:38 +0530

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,8 +64,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin'            => \App\Http\Middleware\Admin::class,
-        'permission'       => \App\Http\Middleware\Permission::class,
-        'users'         => \App\Http\Middleware\Users::class,
+         'users'         => \App\Http\Middleware\Users::class,
         
 
     ];
